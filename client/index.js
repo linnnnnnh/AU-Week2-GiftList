7f74bba7be364520f2d0c88039807c6a07989a71
linnnnnnh <lin-h@hotmail.fr>
2023-09-19 12:47:58 +0200

    final

--- client/index.js
+++ client/index.js
@@ -6,9 +6,31 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+
+  //get the root: 
+  const root = merkleTree.getRoot();
+
+  //get the proof:
+  function getTheProof(theName) {
+    const index = niceList.findIndex(n => n === theName);
+    const p = merkleTree.getProof(index);
+    return p;
+  }
+
+  //invite user to type name: 
+  if (process.argv.length < 3) {
+    console.error('Please type a name to verify as a command-line argument.');
+    process.exit(1);
+  }
+
+  const name = process.argv.slice(2).join(' ');
+  const proof = getTheProof(name);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
   });
 
   console.log({ gift });
